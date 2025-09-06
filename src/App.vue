37623a0b18fee569ba4fe5dfba3de424056846e8
Cauliflower <mccauliflower@protonmail.com>
2025-09-05 20:52:59 -0400

    reset project using new vue packages

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RobotBuilder msg="Welcome to the Robot Builder"/>
+  </div>
+</template>
+
+<script>
+import RobotBuilder from './components/RobotBuilder.vue';
+
+export default {
+  name: 'app',
+  components: {
+    RobotBuilder,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
